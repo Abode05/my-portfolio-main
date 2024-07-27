968fe76149c5a959c85bb20011b29aaf62003b4e
Abode05 <147718633+Abode05@users.noreply.github.com>
2024-07-28 02:44:31 +0300

    Update Project.jsx

--- components/projects/Project.jsx
+++ components/projects/Project.jsx
@@ -1,9 +1,9 @@
 'use client'
+
 import { useState } from 'react'
-import './main.css'
-import { myProjects } from './myProjects'
 import { AnimatePresence, motion } from 'framer-motion'
 import Link from 'next/link'
+import { myProjects } from './myProjects'
 
 const Project = () => {
   const [currentActive, setcurrentActive] = useState('all')
@@ -24,67 +24,80 @@ const Project = () => {
   }
 
   return (
-    <main className="flex relative top-20 ">
-      <h3 className="absolute text-center   w-screen uppercase tracking-[15px] text-gray-500 text-2xl font-bold">
-        projects
+    <main className="flex flex-col items-center gap-10 mt-52 top-40 bg-zinc-800 px-4 relative mb-40">
+      <h3 className="absolute text-center top-16 uppercase tracking-[15px] text-[#8b4ad4] font-bold font-serif text-2xl ">
+        Projects
       </h3>
-      <section className="flex  left-section mt-16 md:ml-16  z-10  flex-wrap ">
+      <section className="flex flex-col md:flex-row items-start gap-2.5 md:ml-16 z-10 mt-32">
         <button
           onClick={() => {
             setcurrentActive('all')
             setArr(myProjects)
           }}
-          className={`${currentActive === 'all' ? 'active' : null} `}
+          className={`bg-[#24252E] w-44 py-3 text-center text-lg capitalize opacity-50 rounded-md transition duration-300 text-beige ${
+            currentActive === 'all'
+              ? 'opacity-100 font-bold border border-[#7251a8] py-3.5'
+              : 'hover:opacity-100 hover:text-[#ccc1dd]'
+          }`}
         >
-          all projects
+          All Projects
         </button>
-
         <button
-          onClick={() => {
-            handleClick('js')
-          }}
-          className={currentActive === 'js' ? 'active' : null}
+          onClick={() => handleClick('js')}
+          className={`bg-[#24252E] w-44 py-3 text-center text-lg capitalize opacity-50 rounded-md transition duration-300 text-beige ${
+            currentActive === 'js'
+              ? 'opacity-100 font-bold border border-[#7251a8] py-3.5'
+              : 'hover:opacity-100 hover:text-[#ccc1dd]'
+          }`}
         >
           JavaScript
         </button>
         <button
-          onClick={() => {
-            handleClick('react')
-          }}
-          className={`${currentActive === 'react' ? 'active' : null}`}
+          onClick={() => handleClick('react')}
+          className={`bg-[#24252E] w-44 py-3 text-center text-lg capitalize opacity-50 rounded-md transition duration-300 text-beige ${
+            currentActive === 'react'
+              ? 'opacity-100 font-bold border border-[#7251a8] py-3.5'
+              : 'hover:opacity-100 hover:text-[#ccc1dd]'
+          }`}
         >
           React & UI
         </button>
       </section>
-      <div className="w-full absolute top-16 bg-[#f7abba]/10 left-0 h-[500px] -skew-y-12 " />
-      <section className=" flex right-section mt-10 md:mt-20">
-        <AnimatePresence>
-          {arr.map((item, index) => {
-            return (
-              <Link href={item.url}>
-                <motion.article
-                  layout
-                  initial={{ transform: 'scale(0.4)' }}
-                  animate={{ transform: 'scale(1)' }}
-                  transition={{ type: 'spring', damping: 8, stiffness: 50 }}
-                  key={index}
-                  className=" card "
-                >
-                  <img
-                    width={266}
-                    src={item.imgPath}
-                    alt=""
-                    className="h-[14rem] object-fill"
-                  />
 
-                  <div style={{ width: '265px' }} className="box">
-                    <h1 className="title">{item.projectTitle}</h1>
-                    <p className="sub-title">{item.desc}</p>
+      <section className="flex flex-wrap justify-center gap-4 mt-10 md:mt-20 w-full">
+        <AnimatePresence>
+          {arr.map((item, index) => (
+            <div key={index} className="w-full sm:w-1/2 lg:w-1/3 xl:w-[30%] p-3">
+              <motion.article
+                layout
+                initial={{ transform: 'scale(0.4)' }}
+                animate={{ transform: 'scale(1)' }}
+                transition={{ type: 'spring', damping: 8, stiffness: 50 }}
+                className="card bg-gradient-to-r
+                 from-[#ffffff0d] to-[#ffffff0d] shadow-md border
+                  border-[#6b61c296] rounded-md transition duration-300
+                   hover:border-blue-500 hover:rotate-1 hover:scale-105 cursor-pointer"
+              >
+                <img
+                  src={item.imgPath}
+                  alt=""
+                  className="object-fill  w-full h-56 rounded-t-md"
+                />
+                <div className="p-4 flex flex-col justify-between h-48 text-white">
+                  <div>
+                    <h1 className="text-xl capitalize">{item.projectTitle}</h1>
+                    <p className="text-sm mt-2 mb-4 w">{item.desc}</p>
                   </div>
-                </motion.article>
-              </Link>
-            )
-          })}
+                  <Link
+                    href={item.url}
+                    className="text-blue-600 text-lg  hover:underline "
+                  >
+                    Visit Website
+                  </Link>
+                </div>
+              </motion.article>
+            </div>
+          ))}
         </AnimatePresence>
       </section>
     </main>
